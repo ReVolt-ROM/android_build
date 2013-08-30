ffd0b16df3ee1f6a0ac447fffac2e97ce0c4a2b7
John Hany <johnhany97@gmail.com>
2013-08-30 18:39:27 +0200

    When I said I missed them, I meant ALL OF THEM !!

--- core/definitions.mk
+++ core/definitions.mk
@@ -1054,7 +1054,7 @@ $(hide) $(PRIVATE_CC) \
 endef
 
 define transform-host-c-to-o-no-deps
-@echo ${CL_PFX}"host C:"${CL_RST}" $(PRIVATE_MODULE) <= $<"
+@echo -e ${CL_PFX}"host C:"${CL_RST}" $(PRIVATE_MODULE) <= $<"
 $(call transform-host-c-or-s-to-o-no-deps, $(PRIVATE_CFLAGS) $(PRIVATE_CONLYFLAGS) $(PRIVATE_DEBUG_CFLAGS))
 endef
 
